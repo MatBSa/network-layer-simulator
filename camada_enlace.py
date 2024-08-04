842f55b1cedfb79aa05ffe4e19ef56054e9e0593
Matheus Barbosa Santos <matheus.bsa@hotmail.com>
2024-08-04 12:35:56 -0300

    Adds method to check parity bit

--- camada_enlace.py
+++ camada_enlace.py
@@ -1,3 +1,6 @@
 # Error Detection using Parity
 def generate_parity(data):
     return data + str(data.count('1') % 2)
+
+def check_parity(data):
+    return data[:-1], data[-1] == str(data[:-1].count('1') % 2)
--- simulador.py
+++ simulador.py
@@ -1,5 +1,5 @@
 from camada_fisica import ask, fsk, plot_signal
-from camada_enlace import generate_parity
+from camada_enlace import generate_parity, check_parity
 
 def main():
     data = "1100101"  # Exemplo de dados binários
@@ -9,6 +9,10 @@ def main():
     # Adicionando Paridade
     data_with_parity = generate_parity(data)
     print(f"Data with Parity: {data_with_parity}")
+    
+    # Verificando Paridade
+    checked_data, parity_valid = check_parity(data_with_parity)
+    print(f"Checked Data: {checked_data}, Parity Valid: {parity_valid}")
 
     #########Camada física
     
